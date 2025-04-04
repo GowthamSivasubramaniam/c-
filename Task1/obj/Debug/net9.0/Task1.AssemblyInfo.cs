1fb1115ebbe148d4252ca0d4a2e9c0f1689a110e
GowthamSivasubramaniam <gowthamsivasubramaniam03@gmail.com>
2025-04-04 16:37:55 +0530

    completed Task1

--- Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4a07eee9d1b462e4c437033d6c1b21a18a78d60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
