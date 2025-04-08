8864208fb8757ba1eb484940bf2b8a8d3339ecef
GowthamSivasubramaniam <gowthamsivasubramaniam03@gmail.com>
2025-04-08 22:36:55 +0530

    Completed Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+class Program
+{
+     static async Task Main(string[] args)
+    {  
+        Console.Write("Enter a number :");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter a number :");
+        double b =  Convert.ToDouble(Console.ReadLine());
+        Task add = Add(a,b);
+        Task sub = Sub(a,b);
+        Task div = Divide(a,b);
+        await Task.WhenAll(add, sub,div);
+        Console.WriteLine("Completed all the tasks");
+    }
+
+    static async Task Add(double a, double b)
+    {
+        await Task.Delay(1000);
+        Console.WriteLine($"Addition: {a + b}");
+    }
+    static async Task Sub(double a, double b)
+    {
+        await Task.Delay(100);
+        Console.WriteLine($"Subtraction: {a - b}");
+    }
+    static async Task Divide(double a, double b)
+    {
+        await Task.Delay(100);
+        try{
+            if(b==0)
+            throw new DivideByZeroException();
+        Console.WriteLine($"Division: {a/b}");
+        }
+        catch(DivideByZeroException ex)
+        {
+            Console.WriteLine("Divisor cannot be zero");
+        }
+    }
+}
---
+++ Task7/Readme.md
@@ -0,0 +1,28 @@
+# Asynchronous Programming and Multi-threadingObjective:Requirements:
+
+## Objective
+- Develop a console application that performs multiple asynchronous operations concurrently.
+- Use `async` and `await` to fetch data from multiple simulated sources (e.g., using `Task.Delay` to mimic API calls).
+- Aggregate the results once all tasks are complete.
+- Handle exceptions that may occur during asynchronous operations.
+
+
+## 📝 Example Output
+```sh
+Enter a number :23
+Enter a number :0
+Divisor cannot be zero
+Subtraction: 23
+Addition: 23
+--
+Enter a number :23
+Enter a number :24
+Division: 0.9583333333333334
+Subtraction: -1
+Addition: 47
+Completed all the tasks
+```
+
+
+
+
